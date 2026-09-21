a482ab6e11b66755efe08492d6e9d15b80a90120
Amanda <contatoamandadcs@gmail.com>
2026-09-21 13:53:06 -0300

    Exercícios

--- calculadora_peso_ideal.java
+++ calculadora_peso_ideal.java
@@ -12,13 +12,16 @@ public class calculadora_peso_ideal {
         altura = Double.parseDouble(JOptionPane.showInputDialog("Digite a altura"));
         sexo = JOptionPane.showInputDialog("Digite o sexo (F ou M)").charAt(0);
         //processamento
-        pesoIdealM = (72.7*altura)-58;
-        pesoIdealF = (62.1*altura) - 44.7;
-        //
-        if (sexo == 'M') {
+        if (sexo=='M' || sexo=='m') {
+            pesoIdealM = (72.7*altura)-58;
             JOptionPane.showMessageDialog(null, String.format("Peso ideal: %.1f ", pesoIdealM));
+            
+        } else if (sexo=='F' || sexo=='f') {
+             pesoIdealF = (62.1*altura) - 44.7;
+             JOptionPane.showMessageDialog(null, String.format("Peso ideal: %.1f ", pesoIdealF));
         }
-        else 
-            JOptionPane.showMessageDialog(null, String.format("Peso ideal: %.1f ", pesoIdealF));
+        else {
+            JOptionPane.showMessageDialog(null, "Sexo inválido!");
+        }
     }
 }
