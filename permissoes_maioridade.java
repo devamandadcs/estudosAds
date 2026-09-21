c3f08c8b5eefd6157d4df4902c901193f773f128
Amanda <contatoamandadcs@gmail.com>
2026-09-21 13:53:21 -0300

    Exercícios

--- permissoes_maioridade.java
+++ permissoes_maioridade.java
@@ -10,16 +10,11 @@ public class permissoes_maioridade {
         //entrada
         idade = Integer.parseInt(JOptionPane.showInputDialog("Digite a sua idade"));
         //condicional
-        if (idade<16) {
-            JOptionPane.showMessageDialog(null, "Não pode votar" +"\n"+ 
-                                                "Não pode dirigir");
-        }
-        else if (idade>=16 && idade<18) {
-            JOptionPane.showMessageDialog(null, "Pode votar" +"\n"+
-                                                "Não pode dirigir");
-        }
+        if (idade>=18)
+            JOptionPane.showMessageDialog(null, "Você pode votar e dirigir.");
+        else if (idade>=16)
+            JOptionPane.showMessageDialog(null, "Você só pode votar.");
         else 
-            JOptionPane.showMessageDialog(null, "Pode votar" + "\n" + 
-                                                "Pode dirigir");
+            JOptionPane.showMessageDialog(null, "Você não pode votar nem dirigir.");
     }
 }
