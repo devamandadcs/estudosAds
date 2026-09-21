1f90457edd3974df5afa545d6be16b240370b697
Amanda <contatoamandadcs@gmail.com>
2026-09-21 14:00:52 -0300

    Exercícios

--- operacoes.java
+++ operacoes.java
@@ -6,32 +6,33 @@ import javax.swing.JOptionPane;
 public class operacoes {
     public static void main(String[] args) {
         //declaração
-        double n1, n2, soma, sub, mult, div, pow;
+        double n1, n2, resposta;
         char operacao;
         //entrada
         n1 = Double.parseDouble(JOptionPane.showInputDialog("Digite o primeiro número"));
         n2 = Double.parseDouble(JOptionPane.showInputDialog("Digite o segundo número"));
-        operacao = JOptionPane.showInputDialog("Digite a operação (símbolo)").charAt(0);
+        operacao = JOptionPane.showInputDialog("Digite a operação: \n+ \n- \n* \n/").charAt(0);
         //processamento
-        soma = n1+n2;
-        sub = n1-n2;
-        mult = n1*n2;
-        div = n1/n2;
-        //condicional
-        if (operacao == '+') {
-            JOptionPane.showMessageDialog(null, "Soma = " + soma);
+        switch (operacao) {
+            case '+':
+                resposta=n1+n2;
+                JOptionPane.showMessageDialog(null, resposta);
+                break;
+            case '-':
+                resposta=n1-n2;
+                JOptionPane.showMessageDialog(null, resposta);
+                break;
+            case '*':
+                resposta=n1*n2;
+                JOptionPane.showMessageDialog(null, resposta);
+                break;
+            case '/':
+                resposta=n1/n2;
+                JOptionPane.showMessageDialog(null, resposta);
+                break;
+            default:
+                JOptionPane.showMessageDialog(null, "Operação inválida!");
         }
-        else if (operacao == '-') {
-            JOptionPane.showMessageDialog(null, "Subtração: " + sub);
-        }
-        else if (operacao == '*') {
-            JOptionPane.showMessageDialog(null, "Multiplicação: " + mult);
-        }
-        else if (operacao == '/') {
-            JOptionPane.showMessageDialog(null, "Divisão: " + div);
-        }
-        else 
-            JOptionPane.showMessageDialog(null, "Operação inválida");
     }
 }
     
